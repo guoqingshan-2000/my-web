4018fbd95b80799e388da8df854ab68731f488fa
郭青山 <guoqingshan@hudunsoft.com>
2024-05-20 10:33:19 +0800

    test

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {" "}
+    {/* React 的严格语法模式，它和 JS 中的 "use strict" 并不相同 */}
+    <div>Explosion!!</div>
+  </React.StrictMode>
+);
